2186da53703a99f82996aeb2b3b85086643234e3
kongxiao <kongx1995@163.com>
2021-08-12 10:52:06 +0800

    修改 HelloWorld 案例

--- demo-helloworld/src/main/java/com/xiao/HelloWorldApplication.java
+++ demo-helloworld/src/main/java/com/xiao/HelloWorldApplication.java
@@ -1,7 +1,13 @@
 package com.xiao;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 /**
  * @Classname com.xiao.HelloWorldApplication
@@ -10,9 +16,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @version: 1.0.0
  */
 @SpringBootApplication
+@Slf4j
 public class HelloWorldApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(HelloWorldApplication.class, args);
-        System.out.println("--------service start success ------");
+    public static void main(String[] args) throws UnknownHostException {
+        ConfigurableApplicationContext context = SpringApplication.run(HelloWorldApplication.class, args);
+        Environment env = context.getBean(Environment.class);
+        String port = env.getProperty("server.port");
+        String ip = InetAddress.getLocalHost().getHostAddress();
+        log.info("\n---------------------------------------------------------\n" +
+                "\tApplication is running! Access address: http://{}:{}" +
+                "\n---------------------------------------------------------\n", ip, port);
     }
 }
